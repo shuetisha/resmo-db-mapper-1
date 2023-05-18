f07ec587b239795657acc862a132ee0c49728520
korhankeser <korhan@resmo.com>
2023-05-18 14:21:20 +0300

    TEST

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/spf13/viper"
-	"log"
 )
 
 type Config struct {
@@ -15,10 +14,11 @@ type Config struct {
 	DSN         string
 	Schedule    string
 	ContextTime string
+	ConfigPath  string
 }
 
 func (c *Config) Validate() error {
-	if c.IngestKey != "" {
+	if c.IngestKey == "" {
 		return errors.New("ingest key is not set")
 	}
 
@@ -30,22 +30,34 @@ func (c *Config) Validate() error {
 }
 
 func (c *Config) ReadConfig(ver string) error {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
+	c.Version = ver
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Println("failed to read config:", err)
-		log.Println("will try to read config from command line arguments")
-	}
-
-	// Check if config variables are set (for command line runner)
 	flag.StringVar(&c.Schedule, "schedule", "", "schedule for running queries")
 	flag.StringVar(&c.ContextTime, "contextTime", "", "timeout duration for connections")
 	flag.StringVar(&c.DSN, "datasourceName", "", "database datasource name")
 	flag.StringVar(&c.IngestKey, "ingestKey", "", "ingestKey of the integration")
+	flag.StringVar(&c.ConfigPath, "configPath", "", "absolute path of the config")
+
 	flag.Parse()
 
-	c.Version = ver
+	if c.DSN != "" && c.IngestKey != "" {
+		if err := c.Validate(); err != nil {
+			return fmt.Errorf("config is not valid: %w", err)
+		}
+
+		return nil
+	}
+
+	viper.SetConfigName("resmo-db-mapper-config")
+	viper.AddConfigPath(".")
+
+	if c.ConfigPath != "" {
+		viper.AddConfigPath(c.ConfigPath)
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read config: %w", err)
+	}
 
 	if err := viper.Unmarshal(c); err != nil {
 		return fmt.Errorf("failed to unmarshal config: %w", err)
